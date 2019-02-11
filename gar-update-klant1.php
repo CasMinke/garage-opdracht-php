1755d3d1346b531ac9a956ccaad9d9ef46d9b874
CasMinke <casminke@gmail.com>
2019-02-11 12:03:34 +0100

    gar-update-klant1 toegevoegd

---
+++ gar-update-klant1.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>gar-update-klant1.php</title>
+</head>
+<body>
+<h1>garage update klant 1</h1>
+<p>dit formulier wordt gebruikt om klantgegevens te wijzigen.</p>
+<form action="gar-create-klant2.php" method="post">
+    welk klantid wilt u wijzigen?
+    <input type="text" name="klantidvak"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
