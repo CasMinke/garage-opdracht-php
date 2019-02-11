3853abcd2e7a2e96a18b586b5c04ca96b8992228
CasMinke <casminke@gmail.com>
2019-02-11 11:58:49 +0100

    gar-search-klant2 toegevoegd

---
+++ gar-search-klant2.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>gar-searcch-klant2.php</title>
+</head>
+<body>
+<h1>garage zoek op klantid 2</h1>
+<p>op klantid gegevens zoeken uit de tabel klanten van de database garage.</p>
+<?php
+$klantid = $_POST["klantidvak"];
+
+require_once "gar-connect.php";
+$klanten = $conn->prepare("select klantid, klantnaam, klantadres, klantpostcode, klantplaats from klant where klantid = :klantid");
+
+$klanten->execute(["klantid" => $klantid]);
+
+echo "<table>";
+foreach ($klanten as $klant){
+    echo "<tr>";
+    echo "<td>" . $klant["klantid"]         . "</td>";
+    echo "<td>" . $klant["klantnaam"]       . "</td>";
+    echo "<td>" . $klant["klantadres"]      . "</td>";
+    echo "<td>" . $klant["klantpostcode"]   . "</td>";
+    echo "<td>" . $klant["klantplaats"]     . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "<a href='gar-menu.php'> terug naar het menu </a>";
+?>
+</body>
+</html>
