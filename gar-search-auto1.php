f4d09f688ae830e4164a0fc1aec890975bc6880b
CasMinke <casminke@gmail.com>
2019-02-14 14:36:07 +0100

    gar-search-auto1.php aangemaakt

---
+++ gar-search-auto1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>gar-search-auto1.php</title>
+</head>
+<body>
+<h1>garage zoek op autokenteken 1</h1>
+<p>
+    dit formulier zoekt een auto op uit de tabel autos van database garage.
+</p>
+<form action="gar-search-auto2.php" method="post">
+    welk autokenteken zoekt u?
+    <input type="text" name="autokentekenvak"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
