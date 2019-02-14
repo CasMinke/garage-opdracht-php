da001d1e9db61d9dbf47fae6018c779aaa7e7535
CasMinke <casminke@gmail.com>
2019-02-14 14:59:14 +0100

    gar-delete-auto1.php aangemaakt

---
+++ gar-delete-auto1.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>gar-delete-auto1.php</title>
+</head>
+<body>
+<h1>garage delete auto 1</h1>
+<p>dit formulier zoekt een auto van de database garage om het te kunnen verwijderen.</p>
+<form action="gar-delete-auto2.php" method="post">
+    welk autokenteken wilt u verwijderen?
+    <input type="text" name="autokentekenvak"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
