2da57d92f477167f0554434173ad72befddb37b4
CasMinke <casminke@gmail.com>
2019-02-14 14:41:52 +0100

    gar-search-auto2.php aangemaakt

---
+++ gar-search-auto2.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>gar-searcch-auto2.php</title>
+</head>
+<body>
+<h1>garage zoek op autokenteken 2</h1>
+<p>op autokenteken gegevens zoeken uit de tabel autos van de database garage.</p>
+<?php
+$autokenteken = $_POST["autokentekenvak"];
+
+require_once "gar-connect.php";
+$autos = $conn->prepare("select autokenteken, automerk, autotype, autokmstand, klantid from auto where autokenteken = :autokenteken");
+
+$autos->execute(["autokenteken" => $autokenteken]);
+
+echo "<table>";
+foreach ($autos as $auto){
+    echo "<tr>";
+    echo "<td>" . $auto["autokenteken"]         . "</td>";
+    echo "<td>" . $auto["automerk"]       . "</td>";
+    echo "<td>" . $auto["autotype"]      . "</td>";
+    echo "<td>" . $auto["autokmstand"]   . "</td>";
+    echo "<td>" . $auto["klantid"]     . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "<a href='gar-menu.php'> terug naar het menu </a>";
+?>
+</body>
+</html>
