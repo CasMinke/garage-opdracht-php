11ef9024e269d23f2e93122b5ccecc443ea865c9
CasMinke <casminke@gmail.com>
2019-02-14 14:44:14 +0100

    gar-update-auto1.php aangemaakt

---
+++ gar-update-auto1.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>gar-update-auto1.php</title>
+</head>
+<body>
+<h1>garage update auto 1</h1>
+<p>dit formulier wordt gebruikt om auto gegevens te wijzigen.</p>
+<form action="gar-update-auto2.php" method="post">
+    welk autokenteken wilt u wijzigen?
+    <input type="text" name="autokentekenvak"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
